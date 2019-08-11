6f94ea507fb82e05e7acc88457e62c6838b35cb7
salarsk1 <ssafarkh@purdue.edu>
2019-08-11 14:26:17 -0400

    Add files via upload

--- query4/solution.py
+++ query4/solution.py
@@ -2,69 +2,48 @@ import sqlite3
 import csv
 con = sqlite3.connect("air.db")
 cur = con.cursor()
-
-# creat the flights table 
-with open('./hw1/flights.csv','r') as flights_table:
-    dr = csv.DictReader(flights_table, delimiter=',')
-    cols = dr.fieldnames[:-1]
-    to_db = [tuple([i[j] for j in cols]) for i in dr]
-
-cur.execute("drop table if exists flights;")
-cur.execute('''CREATE TABLE flights (
-            YEAR number, MONTH number, DAY_OF_MONTH number,
-            DAY_OF_WEEK number, OP_UNIQUE_CARRIER text, TAIL_NUM text, 
-            OP_CARRIER_FL_NUM number, ORIGIN_AIRPORT_ID number, 
-            DEST_AIRPORT_ID number, CRS_DEP_TIME number, DEP_TIME number,
-            DEP_DELAY number, CRS_ARR_TIME number, ARR_TIME number, 
-            ARR_DELAY number, CANCELLED number, CANCELLATION_CODE text, 
-            CRS_ELAPSED_TIME number, ACTUAL_ELAPSED_TIME number, 
-            AIR_TIME number, DISTANCE number, CARRIER_DELAY number, 
-            WEATHER_DELAY number, NAS_DELAY number, SECURITY_DELAY number,
-            LATE_AIRCRAFT_DELAY number
-            );''')
-cur.executemany('''INSERT INTO flights VALUES 
-                   (?,?,?,?,?,?,?,?,?,?,?,?,?,
-                    ?,?,?,?,?,?,?,?,?,?,?,?,?);''', to_db)
-
-# create the airlines table
-with open('./hw1/airlines.csv') as airlines_table:
-    dr = csv.DictReader(airlines_table, delimiter=',')
-    cols = dr.fieldnames
-    to_db = [tuple([i[j] for j in cols]) for i in dr]
-
-cur.execute("drop table if exists airlines;")
-cur.execute('''CREATE TABLE airlines (
-            OP_UNIQUE_CARRIER text,
-            FULL_OP_UNIQUE_CARRIER text);''')
-cur.executemany('''INSERT INTO airlines VALUES 
-                   (?, ?);''', to_db)
-
-# create the airports table
-with open('./hw1/airports.csv') as airports_table:
-    dr = csv.DictReader(airports_table, delimiter=',')
-    cols = dr.fieldnames
-    to_db = [tuple([i[j] for j in cols]) for i in dr]
-
-cur.execute("drop table if exists airports;")
-cur.execute('''CREATE TABLE airports (
-            AIRPORT_ID number,
-            FULL_AIRPORT_ID text);''')
-cur.executemany('''INSERT INTO airports VALUES 
-                   (?, ?);''', to_db)
-
-# create the cancellation table
-with open('./hw1/cancellations.csv') as cancellations_table:
-    dr = csv.DictReader(cancellations_table, delimiter=',')
-    cols = dr.fieldnames
-    to_db = [tuple([i[j] for j in cols]) for i in dr]
-
-cur.execute("drop table if exists cancellations;")
-cur.execute('''CREATE TABLE cancellations (
-            CODE text,
-            CODE_DESCRIPTION text);''')
-cur.executemany('''INSERT INTO cancellations VALUES 
-                   (?, ?);''', to_db)
-
-con.commit()
+cur.execute('''CREATE UNIQUE INDEX IF NOT EXISTS flights_ind ON flights(ORIGIN_AIRPORT_ID, DEST_AIRPORT_ID, MONTH, DAY_OF_MONTH, OP_CARRIER_FL_NUM, CRS_DEP_TIME, DEP_TIME);''')
+cur.execute('''SELECT DEP_DELAY, ARR_DELAY \
+               FROM flights \
+               inner join airports on  flights.ORIGIN_AIRPORT_ID = airports.AIRPORT_ID \
+               inner join airlines on  flights.OP_UNIQUE_CARRIER  = airlines.OP_UNIQUE_CARRIER \
+               where airports.FULL_AIRPORT_ID = "Chicago, IL: Chicago O'Hare International" and airlines.FULL_OP_UNIQUE_CARRIER = "Delta Air Lines Inc."
+               ORDER BY DEP_DELAY ASC, ARR_DELAY ASC; ''')
 
 
+p3 = cur.fetchall()
+with open('q3.txt', 'w') as f:
+    for row in p3:
+        if row[0] != '' and row[1]!='':
+            f.write(f"{row[0]}.0,{row[1]}.0\n")
+        #     f.write(', '.join(i for i in row) + '\n')
+
+cur.execute('''SELECT DAY_OF_WEEK, count(*)
+               FROM flights 
+               GROUP BY DAY_OF_WEEK''')
+
+p4 = cur.fetchall()
+with open('q4.txt', 'w') as f:
+    for row in p4:
+        f.write(', '.join(str(i) for i in row) + '\n')
+
+
+cur.execute('''SELECT MONTH, DAY_OF_MONTH, count(*)
+               from flights
+               inner join airports on flights.DEST_AIRPORT_ID = airports.AIRPORT_ID
+               where airports.FULL_AIRPORT_ID = "Honolulu, HI: Daniel K Inouye International"
+               GROUP BY MONTH, DAY_OF_MONTH;''')
+
+p5 = cur.fetchall()
+with open('q5.txt', 'w') as f:
+    for row in p5:
+        f.write(f"{row[0]},{row[1]},{row[2]}\n")
+
+cur.execute('''SELECT MONTH, DAY_OF_MONTH, SUM(WEATHER_DELAY)
+               from flights
+               GROUP BY MONTH, DAY_OF_MONTH ''')
+
+p6 = cur.fetchall()
+with open('q6.txt', 'w') as f:
+    for row in p6:
+        f.write(f"{row[0]},{row[1]},{row[2]}\n")
